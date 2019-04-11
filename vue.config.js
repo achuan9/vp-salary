ad21630013665b756265b5cd990c4b8723bc94a8
shengchuang <sheng.chuang@vpclub.cn>
2019-04-11 19:27:55 +0800

    publish: modify path

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: "/vp-salary/dist",
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.json', '.vue'],
+    },
+  },
+  // 设为false打包时不生成.map文件
+  productionSourceMap: false
+};
