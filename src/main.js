7d68fa759958e7e1c1589a196c3e16b3f40a5929
shengchuang <sheng.chuang@vpclub.cn>
2019-04-12 10:03:23 +0800

    feat: 按需引入组件

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue';
 import '@babel/polyfill';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+
+import "./element-ui";
 
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
